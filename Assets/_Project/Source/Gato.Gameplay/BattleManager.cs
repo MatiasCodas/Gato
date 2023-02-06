83bf8a0317fc4ebe1754134a1c63682b16495d1c
Cronokami <Cronokami@hotmail.com>
2023-02-05 23:04:08 -0300

    Enemy Spawn working
    And trying to start with the hud but failing

--- Assets/_Project/Source/Gato.Gameplay/BattleManager.cs
+++ Assets/_Project/Source/Gato.Gameplay/BattleManager.cs
@@ -1,33 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+//using TMPro;
+
 
 namespace Gato.Gameplay
 {
     public class BattleManager : MonoBehaviour
     {
         public static BattleManager Instance;
-        public BattleStats BattleStats;
+        
         public GameObject BasicEnemy;
 
         public List<Transform> SpawnPositions;
-        private List<Transform> _unbundledSpawn;
+        private List<Vector2> _unbundledSpawn;
         private List<GameObject> _activeEnemies;
         public float WaveSize;
         public float SpawnTime;
+
+
+        [Header( "HUD info")]
+        public BattleStats BattleStats;
+       // public TMP_Text EnemiesDefeatedText;
 
         private void Start()
         {
             Instance = this;
             StartCoroutine(SpawnNew());
             BattleStats.StartAtTime = 0;
-            _unbundledSpawn = SpawnPositions;
+            GetSpawnPositions();
             _activeEnemies = new List<GameObject>();
         }
 
         private void Update()
         {
-            
+       //     EnemiesDefeatedText.text = "Enemies defeated: " + BattleStats.BasicEnemiesDefeated;
         }
 
         public void EnemyDestroyed()
@@ -42,31 +50,40 @@ namespace Gato.Gameplay
                 }
             }
 
-            if (_activeEnemies.Count == 0)
+            if (_activeEnemies.Count <= 1)
             {
                 StartCoroutine(SpawnNew());
             }
 
         }
+
+        private void GetSpawnPositions()
+        {
+            _unbundledSpawn = new List<Vector2>();
+            for (int i = 0; i < SpawnPositions.Count; i++)
+            {
+                _unbundledSpawn.Add(SpawnPositions[i].position);
+            }
+        }
         
 
         private IEnumerator SpawnNew()
         {
-            
-
             yield return new WaitForSeconds(SpawnTime);
             if (BattleStats.StartAtTime == 0) BattleStats.StartAtTime = Time.time;
 
             int randomPosition;
-            for (int i = 1; i < WaveSize; i++)
+            for (int i = 0; i < WaveSize; i++)
             {
-                randomPosition = Random.Range(0, _unbundledSpawn.Count-1);
-                GameObject instance = Instantiate(BasicEnemy, _unbundledSpawn[randomPosition].position, Quaternion.identity);
+                randomPosition = Random.Range(0, _unbundledSpawn.Count);
+                //Debug.Log(randomPosition);
+                GameObject instance = Instantiate(BasicEnemy, _unbundledSpawn[randomPosition], Quaternion.identity);
                 _activeEnemies.Add(instance);
                 _unbundledSpawn.RemoveAt(randomPosition);
-                Debug.Log(randomPosition);
             }
-            _unbundledSpawn = SpawnPositions;
+            GetSpawnPositions();
+            Debug.Log(_unbundledSpawn.Count);
+            Debug.Log(SpawnPositions.Count);
         }
 
 
