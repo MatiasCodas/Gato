45bcb7af54a6cc19aeedf72adac178fc37310280
Paula Picolott <paula_picolott@hotmail.com>
2023-08-10 20:11:38 -0300

    Inventory item drop mechanics implemented

--- Assets/_Project/Source/Gato.Gameplay/Weapon/Inventory.cs
+++ Assets/_Project/Source/Gato.Gameplay/Weapon/Inventory.cs
@@ -3,19 +3,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 namespace Gato.Gameplay
 {
     public class Inventory : MonoBehaviour, IDropHandler
     {
+        [SerializeField] private InputActionReference _mousePosInput;
+        [SerializeField] private InputActionReference _gamepadStickPosInput;
+        [SerializeField] private InputActionReference _dropItemInput;
+        [SerializeField] private Transform _sceneObjects;
+
         private void OnEnable()
         {
+            _dropItemInput.action.started += DropItemOnFloor;
             InventoryItem.PutInInventory += AddToInventory;
         }
 
         private void OnDisable()
         {
+            _dropItemInput.action.started -= DropItemOnFloor;
             InventoryItem.PutInInventory -= AddToInventory;
+        }
+
+        private void DropItemOnFloor(InputAction.CallbackContext context)
+        {
+            if (transform.childCount > 0)
+            {
+                Transform child = transform.GetChild(0);
+                child.transform.SetParent(_sceneObjects);
+
+                Vector2 newPos = Vector2.zero;
+
+                if (_gamepadStickPosInput.action.IsPressed())
+                {
+                    _mousePosInput.action.Disable();
+                    newPos = Camera.main.ScreenToWorldPoint(_gamepadStickPosInput.action.ReadValue<Vector2>());
+                }
+                else if (!_gamepadStickPosInput.action.IsPressed())
+                {
+                    _mousePosInput.action.Enable();
+                    newPos = Camera.main.ScreenToWorldPoint(_mousePosInput.action.ReadValue<Vector2>());
+                }
+
+                if (_mousePosInput.action.IsPressed())
+                {
+                    newPos = Camera.main.ScreenToWorldPoint(_mousePosInput.action.ReadValue<Vector2>());
+                }
+
+                child.transform.position = newPos;
+                child.transform.localScale = new Vector3(.0065f, .0065f, .0065f);
+            }
         }
 
         private void AddToInventory(Transform itemTransform)
