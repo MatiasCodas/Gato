2330349b6d91e674946189e7cb0b9f5c726c2e03
Cronokami <Cronokami@hotmail.com>
2023-08-22 23:18:42 -0300

    Conflict in input for main menu adjustment

--- Assets/_Project/Source/Gato.UI/MainMenuScreen.cs
+++ Assets/_Project/Source/Gato.UI/MainMenuScreen.cs
@@ -12,15 +12,43 @@ namespace Gato.UI
         private string _nextSceneName;
         [SerializeField]
         private Button _playButton;
+        [SerializeField]
+        private Button _optionsButton;
+        [SerializeField]
+        private Button _creditsButton;
+        [SerializeField]
+        private Button[] _returnToMainButton;
+
+        private CordelPositioning _cordelPositioning;
 
         private void Awake()
         {
+            _cordelPositioning = GetComponent<CordelPositioning>();
             _playButton.onClick.AddListener(HandlePlayButtonPressed);
+            _optionsButton.onClick.AddListener(HandleOptionsButtonPressed);
+            _creditsButton.onClick.AddListener(HandleCreditsButtonPressed);
+            for (int i = 0; i < _returnToMainButton.Length; i++)
+            {
+                _returnToMainButton[i].onClick.AddListener(HandleReturnToMainButtonPressed);
+            }
         }
 
         private void HandlePlayButtonPressed()
         {
             SceneManager.LoadScene(_nextSceneName);
+        }
+
+        private void HandleReturnToMainButtonPressed()
+        {
+            _cordelPositioning.SetIndex(0);
+        }
+        private void HandleOptionsButtonPressed()
+        {
+            _cordelPositioning.SetIndex(1);
+        }
+        private void HandleCreditsButtonPressed()
+        {
+            _cordelPositioning.SetIndex(2);
         }
     }
 }
