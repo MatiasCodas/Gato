98667d617e594e2e46bcd34120037dccddfe6c29
Cronokami <Cronokami@hotmail.com>
2024-03-28 01:33:56 -0300

    Dançarino com animações

--- Assets/_Project/Source/Gato.Gameplay/Objects/TinyDancer.cs
+++ Assets/_Project/Source/Gato.Gameplay/Objects/TinyDancer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine.Unity;
 
 namespace Gato.Gameplay
 {
@@ -9,10 +10,37 @@ namespace Gato.Gameplay
         public SpriteRenderer SpriteRenderer;
         public Color BlessColor;
         public Collider2D Collider;
+
+        [SerializeField]
+        private SkeletonAnimation _spineSkel;
+        public Spine.AnimationState AnimationState;
+        public Spine.Skeleton Skeleton;
+
+        [SpineAnimation]
+        public string Idle, Saved;
+
+        public bool DebugBless;
+
+        private void Start()
+        {
+            AnimationState = _spineSkel.AnimationState;
+            Skeleton = _spineSkel.skeleton;
+        }
         private void Bless()
         {
-            SpriteRenderer.color = BlessColor;
+           // SpriteRenderer.color = BlessColor;
             Collider.enabled = false;
+            AnimationState.SetAnimation(0, Saved, false);
+            DebugBless = false;
+        }
+
+
+        private void Update()
+        {
+            if(DebugBless)
+            {
+                Bless();
+            }
         }
     }
 }
